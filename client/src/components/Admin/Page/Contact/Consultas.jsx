760a8b11af0e8f0ef682c25b345cf96126312171
Jose Gregorio Romero Mendoza <jgromerom89@gmail.com>
2024-08-28 11:02:40 -0300

    update
    panel de consultas de contacto

--- client/src/components/Admin/Page/Contact/Consultas.jsx
+++ client/src/components/Admin/Page/Contact/Consultas.jsx
@@ -80,12 +80,12 @@ export default function Consultas() {
                   <td className="whitespace-nowrap px-4 py-2 text-gray-700">{item.Subject}</td>
                   <td className="whitespace-nowrap px-4 py-2 text-gray-700">{item.Message}</td>
                   <td className="whitespace-nowrap px-4 py-2">
-                    <button
+                    {/* <button
                       onClick={() => setEditForm(item)}
                       className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700 mr-2"
                     >
                       Editar
-                    </button>
+                    </button> */}
                     <button
                       onClick={() => setDeleteFormId(item.id)}
                       className="inline-block rounded bg-red-600 px-4 py-2 text-xs font-medium text-white hover:bg-red-700"
